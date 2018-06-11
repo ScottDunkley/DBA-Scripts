3476ebabab5ff17538fb172c913104f631789c39
Keith J Baugh <keith.baugh@hays.com>
2018-06-11 15:04:04 +0100

    Moving scripts to folder spcific structure

--- redef_abort.sql
+++
@@ -1,6 +0,0 @@
-BEGIN
-DBMS_REDEFINITION.abort_redef_table(
-uname      => 'ECRM_UK',
-orig_table => 'EMAIL',
-int_table  => 'EMAIL_INT');
-END;
--- redef_check_noPK.sql
+++
@@ -1,7 +0,0 @@
--- First lets do the redef check to make sure we can redef
-undefine SCHEMA_NAME
-undefine SOURCE_TABLENAME
-BEGIN
-  DBMS_REDEFINITION.CAN_REDEF_TABLE ('&&SCHEMA_NAME', '&&SOURCE_TABLENAME', DBMS_REDEFINITION.CONS_USE_PK);
-END;
-/
